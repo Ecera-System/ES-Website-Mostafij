5a867c2e0f15172d61a7fa01ea8678341cd870f4
Mostafij Mozumdar <91069030+mostafijmf@users.noreply.github.com>
2023-03-28 22:10:04 +0600

    Delete server directory

--- server/utils/errorHandler.js
+++
@@ -1,6 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-    if (err.name === 'MulterError') return res.status(413).send("Maximum upload file size: 10 MB");
-    res.status(500).send(err.message);
-};
-
-module.exports = errorHandler;
